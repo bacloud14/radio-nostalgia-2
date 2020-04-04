80373ba514b032bb026235b822310dcd58f28b4f
bacloud14 <bacloud14@gmail.com>
2020-04-04 11:42:53 +0200

    refactor

--- exoplayer-codelab-00/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
+++ exoplayer-codelab-00/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -15,14 +15,13 @@
  */
 package com.example.exoplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -30,10 +29,8 @@ import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.MergingMediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.source.SingleSampleMediaSource;
-import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -41,20 +38,13 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.Util;
 
-import org.apache.commons.io.IOUtils;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
@@ -65,20 +55,38 @@ import java.util.Locale;
  */
 public class PlayerActivity extends AppCompatActivity {
 
+    final String language = Locale.getDefault().getLanguage();
     private PlayerView playerView;
     private boolean playWhenReady = true;
     private int currentWindow = 0;
     private long playbackPosition = 0;
-    final String language = Locale.getDefault().getLanguage();
     private Uri subtitleUri, videoUriEN, videoUriFR, videoUriAR;
+    private ArrayList<Sample> samples = new ArrayList<Sample>();
+    private SimpleExoPlayer player;
+
+    private static HashMap<String, String> getQueryMap(String queryString, String charsetName) throws UnsupportedEncodingException {
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        String[] fields = queryString.split("&");
+
+        for (String field : fields) {
+            String[] pair = field.split("=");
+            if (pair.length == 2) {
+                String key = pair[0];
+                String value = URLDecoder.decode(pair[1], charsetName).replace('+', ' ');
+                map.put(key, value);
+            }
+        }
+
+        return map;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         playerView = findViewById(R.id.video_view);
     }
-
-    private SimpleExoPlayer player;
 
     private void initializePlayer() throws IOException {
         if (android.os.Build.VERSION.SDK_INT > 9) {
@@ -120,66 +128,46 @@ public class PlayerActivity extends AppCompatActivity {
         // player = ExoPlayerFactory.newSimpleInstance(this);
         playerView.setPlayer(player);
 
+        initURLs();
         // Uri uri = Uri.parse(getString(R.string.media_url_dash));
         DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this,
-                Util.getUserAgent(this, "exo-demo"));
+                Util.getUserAgent(this, "radio-meditation"));
+        ArrayList<MediaSource> mediaSources = new ArrayList<MediaSource>();
+        ConcatenatingMediaSource playlist =
+                new ConcatenatingMediaSource();
 
-        MediaSource mediaSourceEN = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(videoUriEN);
-        MediaSource mediaSourceFR = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(videoUriFR);
-        MediaSource mediaSourceAR = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(videoUriAR);
-
-        MediaSource[] mediaSourcesEN = new MediaSource[2]; //The Size must change depending on the Uris
-        mediaSourcesEN[0] = mediaSourceEN; // uri
-        SingleSampleMediaSource subtitleSourceEN = new SingleSampleMediaSource(subtitleUri, dataSourceFactory,
-                Format.createTextSampleFormat(null, MimeTypes.APPLICATION_SUBRIP, Format.NO_VALUE, "en", null),
-                C.TIME_UNSET);
-        mediaSourcesEN[1] = subtitleSourceEN;
-        //mediaSourcesEN = new MergingMediaSource(mediaSourcesEN, subtitleSourceEN);
-        //////////////////////////////////////////////////////////
-        MediaSource[] mediaSourcesFR = new MediaSource[2]; //The Size must change depending on the Uris
-        mediaSourcesFR[0] = mediaSourceFR; // uri
-        SingleSampleMediaSource subtitleSourceFR = new SingleSampleMediaSource(subtitleUri, dataSourceFactory,
-                Format.createTextSampleFormat(null, MimeTypes.APPLICATION_SUBRIP, Format.NO_VALUE, "en", null),
-                C.TIME_UNSET);
-        mediaSourcesFR[1] = subtitleSourceFR;
-        //mediaSourceFR = new MergingMediaSource(mediaSourcesFR);
-        //////////////////////////////////////////////////////////
-        MediaSource[] mediaSourcesAR = new MediaSource[2]; //The Size must change depending on the Uris
-        mediaSourcesAR[0] = mediaSourceAR; // uri
-        SingleSampleMediaSource subtitleSourceAR = new SingleSampleMediaSource(subtitleUri, dataSourceFactory,
-                Format.createTextSampleFormat(null, MimeTypes.APPLICATION_SUBRIP, Format.NO_VALUE, "en", null),
-                C.TIME_UNSET);
-        mediaSourcesAR[1] = subtitleSourceAR;
-        //mediaSourcesAR = new MergingMediaSource(mediaSourcesAR);
-        //////////////////////////////////////////////////////////
-        ConcatenatingMediaSource concatenatedSource =
-                new ConcatenatingMediaSource(mediaSourceEN, mediaSourceFR, mediaSourceAR);
-        player.setPlayWhenReady(playWhenReady);
-        player.seekTo(currentWindow, playbackPosition);
-        player.prepare(concatenatedSource, false, false);
-
-    }
-
-    private static HashMap<String, String> getQueryMap(String queryString, String charsetName) throws UnsupportedEncodingException {
-        HashMap<String, String> map = new HashMap<String, String>();
-
-        String[] fields = queryString.split("&");
-
-        for (String field : fields) {
-            String[] pair = field.split("=");
-            if (pair.length == 2) {
-                String key = pair[0];
-                String value = URLDecoder.decode(pair[1], charsetName).replace('+', ' ');
-                map.put(key, value);
-            }
+        for (Sample sample: samples){
+            MediaSource OneMediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(sample.url);
+            MediaSource[] oneMediaSourceWithSub = new MediaSource[2];
+            oneMediaSourceWithSub[0] = OneMediaSource;
+            SingleSampleMediaSource oneSubtitleSource = new SingleSampleMediaSource(subtitleUri, dataSourceFactory,
+                    Format.createTextSampleFormat(null, MimeTypes.APPLICATION_SUBRIP, Format.NO_VALUE, "en", null),
+                    C.TIME_UNSET);
+            oneMediaSourceWithSub[1] = oneSubtitleSource;
+            mediaSources.add(OneMediaSource);
+            playlist = new ConcatenatingMediaSource(playlist, OneMediaSource);
         }
 
-        return map;
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        // player.prepare(concatenatedSource, false, false);
+        player.prepare(playlist, false, false);
+    }
+
+    private void initURLs() {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String date = df.format(new Date());
+        int i = 0;
+        for (String cat: Sample.categories){
+            Uri videoUri = Uri.parse(String.format("https://source-audio-mixer.s3.us-east-2.amazonaws.com/mixed/%s/%s/mixed.mp3", cat, date));
+            samples.add(new Sample(Sample.titles[i], videoUri, Sample.descriptions[i]));
+            i++;
+        }
     }
 
     private MediaSource buildMediaSource(Uri uri) {
         DataSource.Factory dataSourceFactory =
-                new DefaultDataSourceFactory(this, "exoplayer-codelab");
+                new DefaultDataSourceFactory(this, "radio-meditation");
         return new ProgressiveMediaSource.Factory(dataSourceFactory)
                 .createMediaSource(uri);
     }
@@ -187,14 +175,7 @@ public class PlayerActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-        String date = df.format(new Date());
 
-        videoUriEN = Uri.parse(String.format("https://source-audio-mixer.s3.us-east-2.amazonaws.com/mixed/EN/%s/mixed.mp3", date));
-        videoUriFR = Uri.parse(String.format("https://source-audio-mixer.s3.us-east-2.amazonaws.com/mixed/FR/%s/mixed.mp3", date));
-        videoUriAR = Uri.parse(String.format("https://source-audio-mixer.s3.us-east-2.amazonaws.com/mixed/AR/%s/mixed.mp3", date));
-
-        subtitleUri = Uri.parse("https://raw.githubusercontent.com/andreyvit/subtitle-tools/master/sample.srt");
         if (Util.SDK_INT >= 24) {
             try {
                 initializePlayer();
@@ -202,6 +183,7 @@ public class PlayerActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+
     }
 
     @Override
