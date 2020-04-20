3b2d4010d8ec0bce1f13727cefaaabd88bc74a05
bacloud14 <bacloud14@gmail.com>
2020-04-20 02:43:26 +0200

    Foreground service and Notifications

---
+++ exoplayer-codelab-00/player-lib/src/main/java/radiomeditation/radio1/exoplayer/AudioPlayerActivity.java
@@ -0,0 +1,153 @@
+package radiomeditation.radio1.exoplayer;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+
+import android.net.Uri;
+
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.SingleSampleMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerNotificationManager;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.MimeTypes;
+import com.google.android.exoplayer2.util.Util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AudioPlayerActivity extends Service {
+    public static SimpleExoPlayer player;
+    private Uri subtitleUri, videoUriEN, videoUriFR, videoUriAR;
+    private ArrayList<Sample> samples = new ArrayList<Sample>();
+    private PlayerNotificationManager playerNotificationManager;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        final Context context = this;
+
+        player = ExoPlayerFactory.newSimpleInstance(context, new DefaultTrackSelector());
+
+         initURLs();
+        // Uri uri = Uri.parse(getString(R.string.media_url_dash));
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(context,
+                Util.getUserAgent(context, "radio-meditation"));
+        ArrayList<MediaSource> mediaSources = new ArrayList<MediaSource>();
+        ConcatenatingMediaSource playlist =
+                new ConcatenatingMediaSource();
+
+        for (Sample sample: samples){
+            MediaSource OneMediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(sample.url);
+            MediaSource[] oneMediaSourceWithSub = new MediaSource[2];
+            oneMediaSourceWithSub[0] = OneMediaSource;
+            SingleSampleMediaSource oneSubtitleSource = new SingleSampleMediaSource(subtitleUri, dataSourceFactory,
+                    Format.createTextSampleFormat(null, MimeTypes.APPLICATION_SUBRIP, Format.NO_VALUE, "en", null),
+                    C.TIME_UNSET);
+            oneMediaSourceWithSub[1] = oneSubtitleSource;
+
+            mediaSources.add(OneMediaSource);
+            playlist = new ConcatenatingMediaSource(playlist, OneMediaSource);
+        }
+
+        // player.prepare(concatenatedSource, false, false);
+        player.prepare(playlist);
+        player.setPlayWhenReady(true);
+        playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
+                context, "channelId", android.R.string.cut, android.R.string.cut, new PlayerNotificationManager.MediaDescriptionAdapter() {
+                    @Override
+                    public String getCurrentContentTitle(Player player) {
+                        return samples.get(player.getCurrentWindowIndex()).title;
+                    }
+
+                    @Nullable
+                    @Override
+                    public PendingIntent createCurrentContentIntent(Player player) {
+                        Intent intent = new Intent(context, PlayerActivity.class);
+                        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    }
+
+                    @Nullable
+                    @Override
+                    public String getCurrentContentText(Player player) {
+                        return samples.get(player.getCurrentWindowIndex()).description;
+                    }
+
+                    @Nullable
+                    @Override
+                    public Bitmap getCurrentLargeIcon(Player player, PlayerNotificationManager.BitmapCallback callback) {
+                        return null;
+                    }
+                }
+        );
+
+        playerNotificationManager.setNotificationListener(new PlayerNotificationManager.NotificationListener() {
+            @Override
+            public void onNotificationStarted(int notificationId, Notification notification) {
+                startForeground(notificationId, notification);
+            }
+
+            @Override
+            public void onNotificationCancelled(int notificationId) {
+                stopSelf();
+            }
+        });
+
+        playerNotificationManager.setPlayer(player);
+        Intent dialogIntent = new Intent(this, PlayerActivity.class);
+        dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(dialogIntent);
+
+    }
+    private void initURLs() {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String date = df.format(new Date());
+        int i = 0;
+        for (String cat: Sample.categories){
+            Uri videoUri;
+            if(Sample.mixing[i]){
+                videoUri = Uri.parse(String.format("https://source-audio-mixer.s3.us-east-2.amazonaws.com/mixed/%s/%s/mixed.mp3", cat, date));
+            }else{
+                videoUri = Uri.parse(String.format("https://source-audio-mixer.s3.us-east-2.amazonaws.com/playlist/%s/mixed.mp3", cat));
+            }
+
+            System.out.println(videoUri);
+            samples.add(new Sample(Sample.titles[i], videoUri, Sample.descriptions[i], Sample.mixing[i]));
+            i++;
+        }
+    }
+    @Override
+    public void onDestroy() {
+        playerNotificationManager.setPlayer(null);
+        player.release();
+        player = null;
+        super.onDestroy();
+    }
+}
--- exoplayer-codelab-00/player-lib/src/main/java/radiomeditation/radio1/exoplayer/PlayerActivity.java
+++ exoplayer-codelab-00/player-lib/src/main/java/radiomeditation/radio1/exoplayer/PlayerActivity.java
@@ -16,12 +16,15 @@
 package radiomeditation.radio1.exoplayer;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
@@ -30,43 +33,21 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.Format;
-import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.source.SingleSampleMediaSource;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerNotificationManager;
 import com.google.android.exoplayer2.ui.PlayerView;
 
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.Util;
 import com.radiomeditation.exoplayer.R;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Locale;
 
 import android.app.Service.*;
 /**
  * A fullscreen activity to play audio or video streams.
  */
-public class PlayerActivity extends AppCompatActivity implements ShareDialog.ShareDialogListener {
+public class PlayerActivity extends Activity implements ShareDialog.ShareDialogListener {
 
     private static final int PLAYBACK_CHANNEL_ID = 54321;
     private static final int NOTIFICATION_ID = 654321;
@@ -83,37 +64,35 @@ public class PlayerActivity extends AppCompatActivity implements ShareDialog.Sha
     private SimpleExoPlayer player;
     private PlayerNotificationManager playerNotificationManager;
 
-    private static HashMap<String, String> getQueryMap(String queryString, String charsetName) throws UnsupportedEncodingException {
-        HashMap<String, String> map = new HashMap<String, String>();
-
-        String[] fields = queryString.split("&");
-
-        for (String field : fields) {
-            String[] pair = field.split("=");
-            if (pair.length == 2) {
-                String key = pair[0];
-                String value = URLDecoder.decode(pair[1], charsetName).replace('+', ' ');
-                map.put(key, value);
-            }
-        }
-
-        return map;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        Intent intent = new Intent(this, AudioPlayerActivity.class);
+        Util.startForegroundService(this, intent);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        setContentView(R.layout.activity_player);
+        playerView = findViewById(R.id.video_view);
+
         WebView webView = findViewById(R.id.webView);
         // Configure the webview so that the game will load
         WebSettings settings = webView.getSettings();
+        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
         settings.setJavaScriptEnabled(true);
         settings.setAllowFileAccessFromFileURLs(true);
         settings.setAllowUniversalAccessFromFileURLs(true);
         settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         // Load in the game's HTML file
         webView.loadUrl("file:///android_asset/index.html");
-        playerView = findViewById(R.id.video_view);
+
         textViewURL = findViewById(R.id.textView_URL);
         submit = findViewById(R.id.button);
         submit.setOnClickListener(new View.OnClickListener() {
@@ -122,204 +101,13 @@ public class PlayerActivity extends AppCompatActivity implements ShareDialog.Sha
                 openSubmitDialog();
             }
         });
-        final Context context = this;
-//        Context context,
-//        String channelId,
-//        @StringRes int channelName,
-//        @StringRes int channelDescription,
-//        int notificationId,
-//        MediaDescriptionAdapter mediaDescriptionAdapter
 
-        playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
-                context, "channelId", android.R.string.cut, android.R.string.cut, new PlayerNotificationManager.MediaDescriptionAdapter() {
-                    @Override
-                    public String getCurrentContentTitle(Player player) {
-                        return samples.get(player.getCurrentWindowIndex()).title;
-                    }
-
-                    @Nullable
-                    @Override
-                    public PendingIntent createCurrentContentIntent(Player player) {
-                        Intent intent = new Intent(context, PlayerActivity.class);
-                        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    }
-
-                    @Nullable
-                    @Override
-                    public String getCurrentContentText(Player player) {
-                        return samples.get(player.getCurrentWindowIndex()).description;
-                    }
-
-                    @Nullable
-                    @Override
-                    public Bitmap getCurrentLargeIcon(Player player, PlayerNotificationManager.BitmapCallback callback) {
-                        return null;
-                    }
-                }
-                );
-
+        playerView.setPlayer(AudioPlayerActivity.player);
     }
 
     public void openSubmitDialog(){
         ShareDialog shareDialog = new ShareDialog();
-        shareDialog.show(getSupportFragmentManager(), "share dialog");
-    }
-
-
-
-    private void initializePlayer() throws IOException {
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy =
-                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
-        /*
-        URL url = new URL("https://youtube.com/get_video_info?video_id=q-3qus2pvJM");
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.setRequestProperty("Accept-Language", language);
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            StringBuilder builder = new StringBuilder();
-            String line;
-
-            while ((line = reader.readLine()) != null ) builder.append(line);
-
-
-            HashMap<String, String> video = getQueryMap(builder.toString(), "UTF-8");
-            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@"+video);
-
-            //if(video.indexOf("dashmpd") == -1){
-            //    System.out.println("@@@@@@@@@@@@@ fuck");
-            //}
-        } finally {
-            urlConnection.disconnect();
-        }
-         */
-
-        if (player == null) {
-            DefaultTrackSelector trackSelector = new DefaultTrackSelector();
-            trackSelector.setParameters(
-                    trackSelector.buildUponParameters().setMaxVideoSizeSd());
-            player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
-
-        }
-
-        // player = ExoPlayerFactory.newSimpleInstance(this);
-        playerView.setPlayer(player);
-
-        initURLs();
-        // Uri uri = Uri.parse(getString(R.string.media_url_dash));
-        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this,
-                Util.getUserAgent(this, "radio-meditation"));
-        ArrayList<MediaSource> mediaSources = new ArrayList<MediaSource>();
-        ConcatenatingMediaSource playlist =
-                new ConcatenatingMediaSource();
-
-        for (Sample sample: samples){
-            MediaSource OneMediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(sample.url);
-            MediaSource[] oneMediaSourceWithSub = new MediaSource[2];
-            oneMediaSourceWithSub[0] = OneMediaSource;
-            SingleSampleMediaSource oneSubtitleSource = new SingleSampleMediaSource(subtitleUri, dataSourceFactory,
-                    Format.createTextSampleFormat(null, MimeTypes.APPLICATION_SUBRIP, Format.NO_VALUE, "en", null),
-                    C.TIME_UNSET);
-            oneMediaSourceWithSub[1] = oneSubtitleSource;
-            mediaSources.add(OneMediaSource);
-            playlist = new ConcatenatingMediaSource(playlist, OneMediaSource);
-        }
-
-        player.setPlayWhenReady(playWhenReady);
-        player.seekTo(currentWindow, playbackPosition);
-        // player.prepare(concatenatedSource, false, false);
-        player.prepare(playlist, false, false);
-    }
-
-    private void initURLs() {
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-        String date = df.format(new Date());
-        int i = 0;
-        for (String cat: Sample.categories){
-            Uri videoUri;
-            if(Sample.mixing[i]){
-                videoUri = Uri.parse(String.format("https://source-audio-mixer.s3.us-east-2.amazonaws.com/mixed/%s/%s/mixed.mp3", cat, date));
-            }else{
-                videoUri = Uri.parse(String.format("https://source-audio-mixer.s3.us-east-2.amazonaws.com/playlist/%s/mixed.mp3", cat));
-            }
-
-            System.out.println(videoUri);
-            samples.add(new Sample(Sample.titles[i], videoUri, Sample.descriptions[i], Sample.mixing[i]));
-            i++;
-        }
-    }
-
-    private MediaSource buildMediaSource(Uri uri) {
-        DataSource.Factory dataSourceFactory =
-                new DefaultDataSourceFactory(this, "radio-meditation");
-        return new ProgressiveMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(uri);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        if (Util.SDK_INT >= 24) {
-            try {
-                initializePlayer();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        hideSystemUi();
-        if ((Util.SDK_INT < 24 || player == null)) {
-            try {
-                initializePlayer();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @SuppressLint("InlinedApi")
-    private void hideSystemUi() {
-        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (Util.SDK_INT < 24) {
-            releasePlayer();
-        }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (Util.SDK_INT >= 24) {
-            releasePlayer();
-        }
-    }
-
-
-    private void releasePlayer() {
-        if (player != null) {
-            playWhenReady = player.getPlayWhenReady();
-            playbackPosition = player.getCurrentPosition();
-            currentWindow = player.getCurrentWindowIndex();
-            player.release();
-            player = null;
-        }
+        // shareDialog.show(getSupportFragmentManager(), "share dialog");
     }
 
     @Override
